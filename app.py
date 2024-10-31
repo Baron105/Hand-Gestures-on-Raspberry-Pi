86af305cd336365198042c690b70fe2ef824d063
Baron105 <barunparua@gmail.com>
2024-10-31 12:58:12 +0530

    minor fixes

--- app.py
+++ app.py
@@ -258,12 +258,7 @@ def normal_mode():
 # Flask route for video streaming mode
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/video_feed')
-def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 if __name__ == '__main__':
 
