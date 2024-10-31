b1746ed91c14d29c16f693064e0b1fef5621affa
ranjim <ranjimdas09@gmail.com>
2024-10-31 12:33:41 +0530

    Added different mdoes of running the app. Livestream mode to stream it on browser.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,12 @@
 from utils.cvfpscalc import CvFpsCalc
+from utils.tools import (
+    select_mode,
+    calc_bounding_rect,
+    calc_landmark_list,
+    pre_process_landmark,
+    logging_csv,
+    draw_landmarks,
+    draw_bounding_rect,
+    draw_info_text,
+    draw_info
+)
